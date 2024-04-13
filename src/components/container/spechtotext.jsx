6b7bed58ee88de57b5140ca0d76a6887630c98d9
v-aibhavbajaj2520 <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2024-04-13 17:29:26 +0530

    Speech to text

--- src/components/container/spechtotext.jsx
+++ src/components/container/spechtotext.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 
 function SpeechToText() {
   const [transcript, setTranscript] = useState('');
-  
+  const [isRecognitionStarted, setIsRecognitionStarted] = useState(false);
+
   const handleSpeechRecognition = () => {
     const recognition = new window.webkitSpeechRecognition(); // For Chrome
+    recognition.onstart = () => {
+      setIsRecognitionStarted(true);
+    };
+    recognition.onend = () => {
+      setIsRecognitionStarted(false);
+    };
     recognition.onresult = (event) => {
       const result = event.results[0][0].transcript;
       setTranscript(result);
@@ -13,9 +20,13 @@ function SpeechToText() {
   };
 
   return (
-    <div >
-      <button onClick={handleSpeechRecognition}>Start Speech Recognition</button>
-      <p>{transcript}</p>
+    <div className='flex flex-col justify-center items-center h-screen bg-gray-100'>
+      <h1 className="text-3xl font-bold mb-4">Speech to Text Generator</h1>
+      <button className='bg-Teal hover:bg-Teal-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus-opacity-50 mb-4' onClick={handleSpeechRecognition}>
+        {isRecognitionStarted ? 'Speech Recognition Started' : 'Start Speech Recognition'}
+      </button>
+      {isRecognitionStarted && <p className="mb-4">Listening...</p>}
+      <textarea className='p-2 border border-gray-300 rounded w-96 h-48' value={transcript} readOnly />
     </div>
   );
 }
--- src/components/container/textarea.jsx
+++ src/components/container/textarea.jsx
@@ -9,15 +9,18 @@ const TextbookReader = () => {
   };
 
   return (
-    <div >
+    <div className='flex justify-center items-center  bg-gray-100'>
+      <div className='max-w-md w-full p-2 bg-white rounded-lg shadow-md'>
       <textarea
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Enter text here..."
         rows={10}
         cols={50}
+        className='w-full px-3 py-2 placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:border-blue-300'
       />
-      <button onClick={speakText}>Read Text</button>
+      <button className="mt-4 bg-Teal hover:bg-Teal-700 text-white font-bold py-2 px-4 rounded" onClick={speakText}>Read Text</button>
+    </div>
     </div>
   );
 };
